71c11c148bd1ac3b1c964adcf7cab1befd20cc5b
Ghért Bergler König <ghert_k@hotmail.com>
2019-09-09 18:25:37 -0300

    Criação e transição de telas

---
+++ src/controller/ContatoController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import DAO.ContatoDAO;
+
+public class ContatoController {
+    private ContatoDAO contatoDAO;
+
+    public ContatoController() {
+        this.contatoDAO = new ContatoDAO();
+    }
+    
+    
+}
---
+++ src/controller/LoginController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import cifradecesar.CifraDeCesar;
+import DAO.UsuarioDAO;
+
+public class LoginController {
+    private CifraDeCesar cifraDeCesar;
+    private UsuarioDAO usuarioDAO;
+
+    public LoginController() {
+        this.cifraDeCesar = new CifraDeCesar();
+        this.usuarioDAO = new UsuarioDAO();
+    }
+    
+    public boolean efetuarLogin(String login, String senha) {
+        String loginCriptografado = this.cifraDeCesar.criptografar(login);
+        String senhaCriptografada = this.cifraDeCesar.criptografar(senha);
+        
+//        this.usuarioDAO.buscarPorLoginSenha(loginCriptografado, senhaCriptografada);
+        return false;
+    }
+}
